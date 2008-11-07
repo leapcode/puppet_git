1355e6d8c2c9d3a0b1be9266f2dc5c7eadc06827
mh <mh@immerda.ch>
2008-11-07 10:28:28 +0000

    merged module from puzzle

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,24 @@
-# init
+#
+# git module
+#
+# Copyright 2008, Puzzle ITC
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
+
+# modules_dir { \"git\": }
+
+class git {
+    include git::base
+}
+
+class git::base {
+    package{'git':
+        ensure => present,
+    }
+}
