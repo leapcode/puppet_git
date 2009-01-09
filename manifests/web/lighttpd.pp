7d3a421c5c62c4a81d317acccd4a83a719557078
mh <mh@immerda.ch>
2009-01-09 01:52:28 +0000

    fixed config

--- manifests/web/lighttpd.pp
+++ manifests/web/lighttpd.pp
@@ -2,16 +2,6 @@ class git::web::lighttpd {
     include lighttpd 
     include lighttpd::base::git::web
 
-    file{'/etc/lighttpd/lighttpd-gitweb-defaults.conf':
-        source => [ "puppet://$server/files/git/web/${fqdn}/lighttpd-gitweb-defaults.conf",
-                    "puppet://$server/files/git/web/lighttpd-gitweb-defaults.conf",
-                    "puppet://$server/git/web/lighttpd-gitweb-defaults.conf" ],
-        require => Package['lighttpd'],
-        notify => Service['lighttpd'],
-        owner => root, group => 0, mode => 0644;
-    }
-
-
     file{'/etc/lighttpd/lighttpd-gitweb.conf':
         ensure => present,
         require => Package['lighttpd'],
