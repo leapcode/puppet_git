01ab2dc5a871a1becd07a35cb518a4acf3dcae7a
mh <mh@immerda.ch>
2009-01-09 01:38:07 +0000

    fixing lighttpd config

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -40,7 +40,7 @@ define git::web::repo::lighttpd(
         owner => root, group => 0, mode => 0644;
     }
     line{"add_include_of_gitwebrepo_${name}":
-        line => "Include /etc/lighttpd/gitweb.d/${name}.conf",
+        line => "include \"gitweb.d/${name}.conf\"",
         file => "/etc/lighttpd/lighttpd-gitweb.conf",
         require => File['/etc/lighttpd/lighttpd-gitweb.conf'],
         notify => Service['lighttpd'],
