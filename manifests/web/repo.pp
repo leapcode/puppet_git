ab8615d05df97e6f8fd9c0b1bdbcf7439d8b8b0e
mh <mh@immerda.ch>
2010-08-11 14:52:25 +0200

    introduce logmode for webrepos

--- manifests/web/repo.pp
+++ manifests/web/repo.pp
@@ -1,10 +1,15 @@
 # domain: the domain under which this repo will be avaiable
 # projectroot: where the git repos are listened
 # projects_list: which repos to export
+#
+# logmode:
+#   - default: Do normal logging including ips
+#   - anonym: Don't log ips
 define git::web::repo(
   $ensure = 'present',
   $projectroot = 'absent',
   $projects_list = 'absent',
+  $logmode = 'default',
   $sitename = 'absent'
 ){
   if ($ensure == 'present') and (($projects_list == 'absent') or ($projectroot == 'absent')){
@@ -31,12 +36,14 @@ define git::web::repo(
     'lighttpd': {
       git::web::repo::lighttpd{$name:
         ensure => $ensure,
+        logmode => $logmode,
         gitweb_url => $gitweb_url,
         gitweb_config => $gitweb_config,
       }
     }
     'apache': {
       apache::vhost::gitweb{$gitweb_url:
+        logmode => $logmode,
         ensure => $ensure,
       }
     }
--- manifests/web/repo/lighttpd.pp
+++ manifests/web/repo/lighttpd.pp
@@ -1,6 +1,10 @@
+# logmode:
+#   - default: Do normal logging including ips
+#   - anonym: Don't log ips
 define git::web::repo::lighttpd(
   $ensure = 'present',
   $gitweb_url,
+  $logmode = 'default'
   $gitweb_config
 ){
   if $ensure == 'present' { include git::web::lighttpd }
